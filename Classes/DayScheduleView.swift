4db7d4dc1c79ddcba6565c7642242024666f7d2b
Michael Collins <michael.collins@naked.software>
2018-08-05 22:15:04 -0700

    Create Day Schedule View
    I began implementing DayScheduleView. I added and configured a
    UIScrollView to display the schedule view in the available viewport and
    support scrolling the schedule up or down.
    
    I created TimeView. TimeView will draw the schedule grid within the
    DayScheduleView and is the view that is actually scrolled by the scroll
    view.
    
    I created the Test playground to let me verify that the control is
    working.

--- Classes/DayScheduleView.swift
+++ Classes/DayScheduleView.swift
@@ -18,7 +18,49 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import UIKit
-
 open class DayScheduleView: UIView {
+  private let scrollView = UIScrollView()
+  private let timeView = TimeView()
+
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    setupView()
+  }
+
+  public required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+
+    setupView()
+  }
+
+  private func setupView() {
+    setupScrollView()
+    setupTimeView()
+  }
+
+  private func setupScrollView() {
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.contentSize = CGSize(width: bounds.width, height: 1920.0)
+    addSubview(scrollView)
+    scrollView.topAnchor
+      .constraint(equalTo: safeAreaLayoutGuide.topAnchor)
+      .isActive = true
+    scrollView.bottomAnchor
+      .constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+      .isActive = true
+    scrollView.leadingAnchor
+      .constraint(equalTo: safeAreaLayoutGuide.leadingAnchor)
+      .isActive = true
+    scrollView.trailingAnchor
+      .constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+      .isActive = true
+  }
+
+  private func setupTimeView() {
+    timeView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.addSubview(timeView)
+    timeView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+    timeView.heightAnchor.constraint(equalToConstant: 1920.0)
+  }
 }
---
+++ Playgrounds/Test.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+import DayScheduleView
+
+PlaygroundPage.current.liveView = DayScheduleView(frame: CGRect(x: 0, y: 0, width: 384, height: 500))
