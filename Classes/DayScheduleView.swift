2cc0230ed01b3d930614a14217225a4834154b08
Michael Collins <michael.collins@naked.software>
2018-09-02 12:57:03 -0700

    Fix Highlight Rendering
    I fixed a big in DayScheduleView with controlling which time period is
    highlighted. I was not properly translating the point from the
    perspective of DayScheduleView to TimeView in order to correctly
    calculate the time period to highlight.

--- Classes/DayScheduleView.swift
+++ Classes/DayScheduleView.swift
@@ -116,11 +116,15 @@ open class DayScheduleView: UIView {
   }
 
   public func hasAppointments(atPoint point: CGPoint) -> Bool {
-    return timeView.hasAppointments(atPoint: point)
+    let scrollViewPoint = convert(point, to: scrollView)
+    let timeViewPoint = scrollView.convert(scrollViewPoint, to: timeView)
+    return timeView.hasAppointments(atPoint: timeViewPoint)
   }
 
   public func highlightTimePeriod(atPoint point: CGPoint) {
-    timeView.highlightTimePeriod(atPoint: point)
+    let scrollViewPoint = convert(point, to: scrollView)
+    let timeViewPoint = scrollView.convert(scrollViewPoint, to: timeView)
+    timeView.highlightTimePeriod(atPoint: timeViewPoint)
   }
 
   public func hideHighlight() {
